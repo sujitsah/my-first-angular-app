5d81f27309978275b1d768d13c2802cc5f996063
sujit sah <sujitsah60@gmail.com>
2017-07-09 17:16:43 +0530

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [ BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
